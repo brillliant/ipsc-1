2112d7d703eb8e185b2f13f3265171245e81fb59
YAUHENI (Eugene) PASHKELEVICH <Yauheni.Pashkelevich@ask.com>
2025-08-22 22:34:58 -0500

    temp. with bugs

--- Assets/scripts/SlideScript.cs
+++ Assets/scripts/SlideScript.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SlideScript : MonoBehaviour {
     private Vector3 localPosition0;
@@ -12,8 +13,13 @@ public class SlideScript : MonoBehaviour {
     
     private Animator sliderAnimator;
     private Boolean sliderAnimationRunning;
+    private bool slideLockedFlag = false;
+    public bool shouldSliderLockTempFlagDebbug;
+    private bool pendingLock;
+    //private JointDrive savedDrive;
     
     private ConfigurableJoint configurableJoint;
+    private Vector3 connectedAnchor;
     
     private void Awake() {
         sliderAnimator = GetComponent<Animator>();
@@ -24,6 +30,10 @@ public class SlideScript : MonoBehaviour {
         pistolScript = GetComponentInParent<PistolScript>();
         
         configurableJoint = GetComponent<ConfigurableJoint>();
+        //savedDrive = configurableJoint.xDrive;
+        
+        configurableJoint.autoConfigureConnectedAnchor = false;
+        connectedAnchor = configurableJoint.connectedAnchor;
     }
 
     public void runSliderAnimation() {
@@ -51,6 +61,10 @@ public class SlideScript : MonoBehaviour {
         transform.localEulerAngles = localRotation0;
         
         DetectSlidePull(); // проверяем, было ли полное передёргивание
+        
+        //todo yp temp log
+        var d = configurableJoint.xDrive;
+        Debug.Log($"spring={d.positionSpring}, damper={d.positionDamper}, maxF={d.maximumForce}, proj={configurableJoint.projectionMode}");
     }
 
     private bool hasTriggeredPullEvent = false;  // флаг, что затвор дёрнули назад
@@ -72,6 +86,13 @@ public class SlideScript : MonoBehaviour {
             if (pistolScript.isRoundInChamber()) {
                 ejectRound(manual); // выброс патрона
             }
+        } else if (slideLockedFlag) {
+            // снятие с задержки при дотяжке назад
+            if (slideLockedFlag && /*!shouldSliderLockTempFlagDebbug*/ !pistolScript.shouldSliderLock()  /* dz >= lockDz + releaseOffset*/) {
+                slideLockedFlag = false;
+                //if (configurableJoint) configurableJoint.xDrive = savedDrive; // вернуть пружину
+                slideLockRangeMove(0);
+            }
         }
     }
 
@@ -89,7 +110,7 @@ public class SlideScript : MonoBehaviour {
     }
 
     public void DetectSlidePull() {
-        //checkSliderLock();
+        checkSlideLock();
         
         if (!sliderAnimationRunning) {
             float dz = transform.localPosition.z - localPosition0.z;
@@ -104,19 +125,61 @@ public class SlideScript : MonoBehaviour {
         }
     }
     
-    private void checkSliderLock() {
-        if (pistolScript.shouldSliderLock()) {
+    /**
+     * при ручном движении слайдера
+     */
+    private void checkSlideLock() {
+        if (!sliderAnimationRunning && !slideLockedFlag && pistolScript.shouldSliderLock()) { // нахуа? если это же я чекаю в onSliderMovedBackAndLittleReturnEvent
             float dz = transform.localPosition.z - localPosition0.z;
             if (dz >= slidePullThreshold) {
-                sliderAnimator.enabled = false;
-
-                JointDrive drive = configurableJoint.xDrive; // получить текущие настройки
-                drive.positionSpring = 0f; // сила пружины
-                configurableJoint.xDrive = drive; // записать обратно
+                pendingLock = true;
             }
         }
     }
 
+    /**
+     * ивент для анимации, когда слайдер ушел назад, и вернулся немного
+     */
+    public void onSliderMovedBackAndLittleReturnEvent() {
+        if (!slideLockedFlag && pistolScript.shouldSliderLock()) {
+            pendingLock = true;
+        }
+    }
+    
+    void FixedUpdate() {
+        if (!pendingLock) return;
+        pendingLock = false;
+        slideLock();
+    }
+
+    public void slideLock() {
+        slideLockedFlag = true;
+        sliderAnimator.enabled = false;
+        sliderAnimationRunning = false;
+
+        slideLockRangeMove(0.008f);
+        //ApplyFrontStop();
+
+        /*JointDrive drive = configurableJoint.xDrive; // получить текущие настройки  //todo надо взять общий. что в переменных
+        drive.positionSpring = 0f; // сила пружины
+        configurableJoint.xDrive = drive; // записать обратно*/
+
+        /*var pos = transform.localPosition;
+        pos.z = -0.01290196f;
+        transform.localPosition = pos;*/
+    }
+    
+    private void slideLockRangeMove(float newLimit) {
+        var connectedAnchorTemp = connectedAnchor;
+        connectedAnchorTemp.z = localPosition0.z + newLimit;                  // или ca.x — по твоей оси
+        configurableJoint.connectedAnchor = connectedAnchorTemp;
+
+        var linearLimit = configurableJoint.linearLimit; 
+        linearLimit.limit = newLimit; 
+        configurableJoint.linearLimit = linearLimit; // ±half => окно [rear, rear+travel]
+        //configurableJoint.targetPosition = Vector3.zero;// цель = центр окна
+    }
+    
     private void tryMoveRoundToChamber() {
         pistolScript.moveRoundFromMagazineToChamber();//пытаемся досылать патрон, если он есть
     }
