1184934c41d4d4ac02e0e3c81c0535fd7bb73b87
YAUHENI (Eugene) PASHKELEVICH <Yauheni.Pashkelevich@ask.com>
2025-08-25 21:29:32 -0500

    чуть тише щелчки. рефактор

--- Assets/scripts/HandScript.cs
+++ Assets/scripts/HandScript.cs
@@ -1,19 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-/*todo на работает. создал. не пригодилось пока. идея чтобы рука знала, есть ли у нее касание с чем либо.*/
 public class HandScript : MonoBehaviour {
-    public GameObject cube;
     private GameObject pistol;
     private PistolScript pistolScript;
-    private GameObject pushHandPoint;
+    //private GameObject pushHandPoint;
 
     void Awake() {
         if (pistol == null) {
             pistol = GameObject.Find("Glock17");
         }
-        pushHandPoint = pistol.transform.Find("pushHandPoint").gameObject;
+        //pushHandPoint = pistol.transform.Find("pushHandPoint").gameObject;
         pistolScript = pistol.GetComponent<PistolScript>();
     }
 
@@ -21,31 +17,6 @@ public class HandScript : MonoBehaviour {
         if (other.gameObject.name == "pushHandPoint" 
             && pistolScript.hasMagazineChild() && !pistolScript.isMagazineLockedInPistol()) {
             pistolScript.getMagazineScript().magazineLock();            
-        }
-        
-        //if (other.gameObject.name == "reloadPoint1" && !isSetUp) {
-            //if (transform.parent is not null) {
-                /*ToggleColor();
-                transform.SetParent(null);
-                gameObject.GetComponent<Rigidbody>().isKinematic = false;
-                gameObject.GetComponent<Rigidbody>().useGravity = true;*/
-            //}
-        //}
-    }
-    
-    //todo удалить позже
-    private bool isColor1Active = true;
-    private Color color1 = Color.magenta;
-    private Color color2 = Color.white;
-    
-    private void ToggleColor() {
-        Renderer renderer = cube.GetComponent<Renderer>();
-        //Renderer renderer = GetComponent<Renderer>();
-        if (renderer != null) {
-            renderer.material.color = isColor1Active ? color1 : color2;
-            isColor1Active = !isColor1Active;
-        } else {
-            Debug.LogWarning("У объекта нет компонента Renderer!");
         }
     }
 }
